3654bc3124e027ab0390c3f4978271f5494e2cf6
Manoel <jmsneto120@gmail.com>
2021-09-29 20:45:22 -0400

    feat: add messages delete endpoint

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Message } from './Message';
 import { MessagesService } from './messages.service';
 
@@ -27,5 +27,10 @@ export class MessagesController {
   @Put(':id')
   update(@Param() params, @Body() message: Message){
     return this.messagesService.update(+params.id, message);
+  }
+
+  @Delete(':id')
+  delete(@Param() params){
+    return this.messagesService.delete(+params.id);
   }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -16,23 +16,30 @@ export class MessagesService {
     }
   ];
 
-  findAll(){
+  findAll(): Message[] {
     return this.messages;
   }
 
-  findById(id: number){
+  findById(id: number): Message {
     return this.messages.find(message => message.id === id);
   }
 
-  create(message: Message){
-    return this.messages.push(message);
+  create(message: Message): Message{
+    this.messages.push(message)
+    return message;
   }
 
-  update(id: number, message: Message){
+  update(id: number, message: Message): Message {
     const index = this.messages.findIndex((message) => message.id === id);
     console.log(index)
     this.messages[index] = message;
 
     return message;
+  }
+
+  delete(id: number): void {
+    const index = this.messages.findIndex((message) => message.id === id);
+
+    delete this.messages[index];
   }
 }
