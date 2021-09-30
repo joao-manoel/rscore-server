d2b16b6bf9cca245f1920b435bee7e7b9d2ad1f7
Manoel <jmsneto120@gmail.com>
2021-09-30 18:30:26 -0400

    feat(csurf): fix remove csurf

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -8,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-  app.use(csurf());
 
   app.useGlobalPipes(new ValidationPipe());
 
