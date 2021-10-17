df0b52d3c27c5e18530480d0cb912b742ad07156
João Manoel <joaomanoel.ms.br@gmail.com>
2021-10-17 00:36:22 -0400

    enable cors all

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.use(helmet());
 
   app.useGlobalPipes(
